aeea5076d0db3805612e8fd0f624fa757f200374
MarcelAndrejak <steam.andrejak@gmail.com>
2021-10-04 15:45:47 +0000

    Add gitignore

--- personalportfolio-project/personalportfolio/settings.py
+++ personalportfolio-project/personalportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4soh%y^&1znze%kt36^z6h@y14d@950ah4qnu^*@ku%q##zue2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['s34l.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = PurePath.joinpath(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = PurePath.joinpath(BASE_DIR,'media')
 
